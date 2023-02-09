50a7ffa9c4784b07c134e0a8c7929d582d85506f
arcyleung <arcyleung@gmail.com>
2023-02-09 15:16:06 -0500

    Added table boilerplate

--- src/App.vue
+++ src/App.vue
@@ -32,39 +32,58 @@
       <el-aside width="200px">
         <div>Instruction Sets</div>
         <el-autocomplete v-model="state1" value-key="_attributes.name" :fetch-suggestions="querySearch" clearable
-          class="inline-input w-50" placeholder="Please Input" @select="handleSelect"><template #default="{ item }">
+          class="inline-input w-50" placeholder="Please Input" @select="handleSelect">
+          <template #default="{ item }">
             <div class="value">{{ item._attributes.name }}</div>
-            <span class="link">{{ item._attributes.tech }}</span>
-          </template></el-autocomplete>
-          <div>Category</div>
+            <!-- <span class="link">{{ item._attributes.tech }}</span> -->
+          </template>
+        </el-autocomplete>
+        <div>Category</div>
         <el-autocomplete v-model="state2" :fetch-suggestions="querySearch" :trigger-on-focus="false" clearable
           class="inline-input w-50" placeholder="Please Input" @select="handleSelect" />
-          <div>Datatypes</div>
+        <div>Datatypes</div>
         <el-autocomplete v-model="state2" :fetch-suggestions="querySearch" :trigger-on-focus="false" clearable
           class="inline-input w-50" placeholder="Please Input" @select="handleSelect" />
       </el-aside>
-      <div>
-        <!-- <el-row class="demo-autocomplete">
-          <el-col :span="12">
+      <div class="flex flex-grow">
+        <el-auto-resizer>
+          <template #default="{ height, width }">
+            <el-table-v2 fixed :columns="columns" :data="data" :width="width" :height="height"
+               :expand-column-key="columns[0].key">
+               
+              <template #overlay>
+                <div class="el-loading-mask" style="display: flex; align-items: center; justify-content: center">
+                  <el-icon class="is-loading" color="var(--el-color-primary)" :size="46">
+                    <loading-icon />
+                  </el-icon>
+                </div>
+              </template>
 
-          </el-col>
-          <el-col :span="12">
-
-          </el-col>
-        </el-row> -->
+              <template #row="props">
+                <Row v-bind="props" />
+              </template>
+              
+            </el-table-v2>
+          </template>
+        </el-auto-resizer>
       </div>
 
     </el-container>
   </div>
 </template>
 
-<script setup>
+<script lang="jsx" setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import { Canvas, Rect } from 'fabric';
+
+import { Loading as LoadingIcon } from '@element-plus/icons-vue'
+
+
 import axios from 'axios';
 
-import { onMounted, ref } from 'vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import { random } from 'lodash';
 
 const state1 = ref('')
 const state2 = ref('');
@@ -81,8 +100,7 @@ const querySearch = (queryString, cb) => {
 }
 const createFilter = (queryString) => {
   return (intrinsic) => {
-    // console.log(intrinsic._attributes.name)
-    // console.log(intrinsic._attributes.name.toLowerCase().indexOf(queryString.toLowerCase()))
+
     return (
       intrinsic._attributes.name.toLowerCase().indexOf(queryString.toLowerCase()) > 0
     )
@@ -93,10 +111,58 @@ const handleSelect = (item) => {
   console.log(item)
 }
 
+const generateColumns = (length = 10, prefix = 'column-', props) => {
+  console.log(intrinsics.value)
+  return Array.from({ length }).map((_, columnIndex) => ({
+    ...props,
+    key: `${prefix}${columnIndex}`,
+    dataKey: `${prefix}${columnIndex}`,
+    title: `Column ${columnIndex}`,
+    width: 150,
+  }))
+}
+
+const generateData = (
+  columns,
+  length = 200,
+  prefix = 'row-'
+) =>
+  Array.from({ length }).map((_, rowIndex) => {
+    return columns.reduce(
+      (rowData, column, columnIndex) => {
+        rowData[column.dataKey] = `Row ${rowIndex} - Col ${columnIndex}`
+        return rowData
+      },
+      {
+        id: `${prefix}${rowIndex}`,
+        parentId: null,
+      }
+    )
+  })
+
+const columns = generateColumns(intrinsics.values)
+const data = ref(generateData(columns, intrinsics.values).map((data) => {
+    data.children = [
+      {
+        id: `${data.id}-detail-content`,
+        detail: "Hello world",
+      },
+    ]
+    return data
+  })
+)
+
+const Row = ({ cells, rowData }) => {
+  if (rowData.detail) return <div class="p-6">{rowData.detail}</div>
+  return cells
+}
+
+Row.inheritAttrs = false
 
 onMounted(async () => {
   const res = await axios.get("/intel_intrinsics.json");
   intrinsics.value = res.data;
+  document.getElementsByClassName("el-table-v2__overlay")[0].remove()
 })
 </script>
 
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import { defineConfig } from 'vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),

499219d3e1bfa05d2ad110a1b78919837443be5d
Arthur Leung <arcyleung@gmail.com>
2023-11-09 23:23:49 -0500

    Add vue config publicPath

--- dist/assets/javascript-c6c6b354.js
+++ dist/assets/javascript-c6c6b354.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-fb6a65ef.js";import"./index-b91b120e.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-156fe132.js";import"./index-e25bc459.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.35.0(71ac097e6155a405f6be52b0b368a04508c31e31)
  * Released under the MIT license

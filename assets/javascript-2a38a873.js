885562267a02d5a456b9f1a28c27ddfeca67cba1
Arthur Leung <arcyleung@gmail.com>
2023-11-09 23:36:38 -0500

    Fix base path in vite

--- assets/javascript-2a38a873.js
+++ assets/javascript-2a38a873.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-156fe132.js";import"./index-e25bc459.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-3364ef06.js";import"./index-ee7917e7.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.35.0(71ac097e6155a405f6be52b0b368a04508c31e31)
  * Released under the MIT license
